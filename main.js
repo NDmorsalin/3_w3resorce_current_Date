6dbcf48da2cd2be6787dbb9033a75b3eee33473d
NDmorsalin <morsalin056@gmail.com>
2020-02-08 09:23:02 +0600

    current date

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 
-console.log("Hello World");
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var dat = date.getDate();
+var day = date.getDay();
+month = (month < 10) ? ("0" + month ) : month;
+dat = (dat < 10) ? ("0" + dat ) : dat;
+
+//mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+var toDay1 = month + "-" + dat 
+            + "-" + year;
+var toDay2 = month + "/" + dat 
+            + "/" + year;
+var toDay3 = dat + "-" +  month
+            + "-" + year;
+  
+            
+console.log(toDay1 + "\t,\n" + 
+            toDay2 + '\t,\n' +
+            toDay3);         
